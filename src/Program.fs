4609128e5b9c4a6b5fa1a77d33dcf838e8eb6741
Stephen Gilboy <stephen@gilboy.io>
2021-11-24 14:05:02 -0600

    Requesting listings from Reddit

--- src/Program.fs
+++ src/Program.fs
@@ -2,25 +2,79 @@
 open System.Net.Http
 open Common
 open RedditAuth
+open RedditApi
 
 let httpClient = new HttpClient()
 
-let run = 
-    async {
-        match getRedditConfig with
+let printListing listing = 
+    printfn "Title: %s\nAuthor: %s\nLink: %s" listing.Title listing.Author listing.Url
+
+let getOAuthGrant config =
+    task {
+        match config with
         | Error msg -> return Error msg
-        | Ok creds ->
-            let! result = getRedditAppOnlyUserlessGrant (creds, httpClient) |> Async.AwaitTask
+        | Ok c -> 
+            let! result = getRedditAppOnlyUserlessGrant (c, httpClient) |> Async.AwaitTask
             match result with
             | Error msg -> return Error msg
-            | Ok auth -> return Ok auth
+            | Ok auth -> return Ok (auth, c.UserAgent)
+    }
+
+let getNewsRequest (input: Result<OAuthGrant * string, string>) = 
+    match input with 
+    | Error msg -> Error msg
+    | Ok p ->
+        let sort = Sort.New
+        let pagination = {
+            Take = After ""
+            Count = (uint)0
+            Limit = (uint16)10
+        }
+        let grant = fst p
+        let userAgent = snd p
+        match getListingRequest grant userAgent "news" sort pagination with
+        | None -> Error "Renew the API Token"
+        | Some req -> Ok req
+
+let getRedditListings (request: Result<HttpRequestMessage, string>) =
+    task {
+        match request with
+        | Error msg -> return Error msg
+        | Ok req -> 
+            let! content = sendRequest (req, httpClient) |> Async.AwaitTask
+            match content with 
+            | Error msg -> return Error msg
+            | Ok s -> 
+                let listings = getListings s
+                return Ok listings
+    }
+
+let showListings listings =
+    match listings with 
+    | Error msg -> Error msg
+    | Ok l -> 
+        for listing in l do
+            printListing listing
+        Ok l
+
+let run = 
+    async {
+            let! grantAndUserAgent = 
+                getRedditConfig
+                |> getOAuthGrant
+                |> Async.AwaitTask
+            let! listings = 
+                getNewsRequest grantAndUserAgent
+                |> getRedditListings
+                |> Async.AwaitTask
+            return listings
     }
 
 
 [<EntryPoint>]
 let main argsv : int =
-    let creds = run |> Async.RunSynchronously
-    match creds with
-    | Ok auth -> printfn "Authenticated" |> ignore
+    let listings = run |> Async.RunSynchronously
+    match showListings listings with
     | Error msg -> printfn "Error\n%s" msg
+    | _ -> printfn "Done"
     0
---
+++ src/RedditApi.fs
@@ -0,0 +1,140 @@
+module RedditApi
+
+open System
+open System.Net.Http
+open System.Text.Json
+open System.Text.Json.Serialization
+open System.Web
+open Common
+open RedditApiResponse
+
+type Kind =
+    | Comment
+    | Account
+    | Link
+    | Message
+    | Subreddit
+    | Award
+    | Unknown
+
+type Sort = 
+    | Controversial
+    | Hot
+    | New
+    | Random
+    | Rising
+    | Top
+
+type Take =
+    | After of string
+    | Before of string
+
+type Pagination = {
+    Take: Take
+    Count: uint
+    Limit: uint16
+}
+
+type Listing = {
+    Id: string
+    Type: Kind
+    Subreddit: string
+    Title: string
+    Author: string
+    Url: string
+    UpVotes: uint32
+    DownVotes: uint32
+    Clicked: bool
+    Saved: bool
+    UpvoteRatio: double
+    NumberOfComments: uint32
+}
+
+let getTypePrefix kind =
+    match kind with
+    | Comment -> "t1_"
+    | Account -> "t2_"
+    | Link -> "t3_"
+    | Message -> "t4_"
+    | Subreddit -> "t5_"
+    | Award -> "t6_"
+    | Unknown -> ""
+
+let getKind prefix =
+    match prefix with
+    | "t1" -> Comment
+    | "t2" -> Account
+    | "t3" -> Link
+    | "t4" -> Message
+    | "t5" -> Subreddit
+    | "t6" -> Award
+    | _ -> Unknown
+
+let getSortString sort =
+    match sort with
+    | Controversial -> "controversial"
+    | Hot -> "hot"
+    | New -> "new"
+    | Random -> "random"
+    | Rising -> "rising"
+    | Top -> "top"
+
+let getFullname listing =
+    let prefix = getTypePrefix listing.Type
+    $"%s{prefix}%s{listing.Id}"
+
+
+let getListingRequest grant userAgent subreddit sort pagination =
+    if isOAuthAccessTokenExpired grant then
+        None
+    else
+    let sortStr = getSortString sort
+    let uri = $"https://oauth.reddit.com/r/%s{subreddit}%s{sortStr}"
+    let uriBuilder = new UriBuilder(uri)
+
+    // Set Query String parameters
+    let query = HttpUtility.ParseQueryString(uriBuilder.Query)     
+    match pagination.Take with
+    | After str -> query["after"] <- str
+    | Before str -> query["before"] <- str
+    query["count"] <- pagination.Count.ToString()
+    query["limit"] <- pagination.Limit.ToString()
+    uriBuilder.Query <- query.ToString()
+    
+    // Create Request
+    let request = new HttpRequestMessage(HttpMethod.Get, uriBuilder.ToString())
+    request.Headers.Add("Authorization", $"Bearer %s{grant.AccessToken}")
+    request.Headers.UserAgent.ParseAdd(userAgent)
+    Some request
+
+let sendRequest (request: HttpRequestMessage, client: HttpClient) =
+    task {
+        let! response = client.SendAsync(request) |> Async.AwaitTask
+        let! clientContent = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        match response.IsSuccessStatusCode with
+        | false ->
+            let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+            return Error $"HTTP: %s{response.ReasonPhrase} Message: %s{content} SENT: %s{clientContent}"
+        | true -> return Ok clientContent
+    }
+
+let getListings (content: string) =
+    let json: ListingResponse = JsonSerializer.Deserialize<ListingResponse>(content)
+    json.data.children
+    |> List.map (fun c ->
+        let kind = getKind c.kind
+        {
+            Id = c.data.id
+            Type = kind
+            Subreddit = c.data.subreddit
+            Title = c.data.title
+            Author = c.data.author
+            Url = c.data.url
+            UpVotes = c.data.ups
+            DownVotes = c.data.downs
+            Clicked = c.data.clicked
+            UpvoteRatio = c.data.upvote_ratio
+            NumberOfComments = c.data.num_comments
+            Saved = c.data.saved
+        }
+    )
